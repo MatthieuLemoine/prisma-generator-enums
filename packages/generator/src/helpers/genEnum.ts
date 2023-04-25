6ad48cb1fa91392f98a67031dac5f6c8394e9869
Matthieu LEMOINE <8517072+MatthieuLemoine@users.noreply.github.com>
2023-04-25 17:32:58 +0200

    feat(generator): generate enum type

--- packages/generator/src/helpers/genEnum.ts
+++ packages/generator/src/helpers/genEnum.ts
@@ -2,5 +2,5 @@ import { DMMF } from '@prisma/generator-helper'
 
 export const genEnum = ({ name, values }: DMMF.DatamodelEnum) => {
   const enumValues = values.map(({ name }) => `${name}: "${name}"`).join(',\n')
-  return `export const ${name} = { \n${enumValues}\n }`
+  return `import type { ${name} as ${name}Type } from '@prisma/client';\n\nexport const ${name}: { [K in ${name}Type]: K } = { \n${enumValues}\n };\n\nexport type ${name} = ${name}Type;`
 }
