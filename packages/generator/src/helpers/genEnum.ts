3b92f66c3b8d16f972b8bf8c4976df4a99292e71
Matthieu LEMOINE <8517072+MatthieuLemoine@users.noreply.github.com>
2023-04-25 10:05:55 +0200

    feat: publish

--- packages/generator/src/helpers/genEnum.ts
+++ packages/generator/src/helpers/genEnum.ts
@@ -2,6 +2,5 @@ import { DMMF } from '@prisma/generator-helper'
 
 export const genEnum = ({ name, values }: DMMF.DatamodelEnum) => {
   const enumValues = values.map(({ name }) => `${name}: "${name}"`).join(',\n')
-
   return `export const ${name} = { \n${enumValues}\n }`
 }
